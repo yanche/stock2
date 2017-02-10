971da540ba4b6a6627875776e4b8f2647a04471e
Yan Chen <yanche@microsoft.com>
2017-02-10 09:48:09 -0800

    f

--- src/nodes/action/control/none.ts
+++ src/nodes/action/control/none.ts
@@ -5,7 +5,7 @@ import Action from '../action';
 
 export const action = new Action<void, void, void>({
     refine: utility.id,
-    validate: utility.id,
+    validate: utility.validate.alwaysTrue,
     resolve: (): bb<void> => {
         return bb.resolve();
     }
