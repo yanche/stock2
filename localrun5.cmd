9baff4424aea944f4efd9b9cd4eb3e08d7ab270e
Yan Chen <yanche.belongs@gmail.com>
2017-07-16 16:11:15 -0700

    startup cmd

---
+++ localrun5.cmd
@@ -0,0 +1,10 @@
+CALL tsc
+CALL grunt dev
+START node "./src/nodes/index.js" d
+START node "./src/nodes/index.js" p -d
+START node "./src/nodes/index.js" w -d
+START node "./src/nodes/index.js" c -d
+START node "./src/nodes/index.js" c -d
+START node "./src/nodes/index.js" c -d
+START node "./src/nodes/index.js" c -d
+START node "./src/nodes/index.js" c -d
